5e9e783951ba1cc568bb41c7ad455950df85eb53
terror-akshat <www.akshatagarwal123klj@gmail.com>
2026-05-31 13:22:17 +0530

    update db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-  const uri = process.env.MONGODB_URI;
+  const uri = "mongodb+srv://satyam87yadav_db_user:%40Satyam7255@ainotes.y7vgwj8.mongodb.net/ai_notes_analyzer?retryWrites=true&w=majority&appName=AInotes";
 
   if (!uri) {
     throw new Error("MONGODB_URI is not configured.");
